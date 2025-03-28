72d3cabbb49c449253ba843e02858d680fc0482a
ALEXSANDER2002 <alex13filipi@gmail.com>
2025-03-28 12:17:27 -0300

    Volta a usar o pacote @djpfs/react-vlibras para implementação do VLibras

--- components/VLibrasComponent.tsx
+++ components/VLibrasComponent.tsx
@@ -1,15 +1,7 @@
 'use client'
 
-import { useEffect } from 'react'
+import VLibras from '@djpfs/react-vlibras'
 
 export default function VLibrasComponent() {
-  useEffect(() => {
-    // @ts-ignore
-    if (window.vlibras) {
-      // @ts-ignore
-      window.vlibras.init()
-    }
-  }, [])
-
-  return null
+  return <VLibras forceOnload={true} />
 } 
