d0d0bf9e0697822705402c99b2af4998cbf070f2
ALEXSANDER2002 <alex13filipi@gmail.com>
2025-03-28 12:19:59 -0300

    Ajusta implementação do VLibras para funcionar em produção na Vercel

--- components/VLibrasComponent.tsx
+++ components/VLibrasComponent.tsx
@@ -1,7 +1,16 @@
 'use client'
 
+import { useEffect } from 'react'
 import VLibras from '@djpfs/react-vlibras'
 
 export default function VLibrasComponent() {
+  useEffect(() => {
+    // @ts-ignore
+    if (window.vlibras) {
+      // @ts-ignore
+      window.vlibras.init()
+    }
+  }, [])
+
   return <VLibras forceOnload={true} />
 } 
