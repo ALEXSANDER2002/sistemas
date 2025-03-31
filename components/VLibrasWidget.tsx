da3b20544c39d5135e47ad5a0bc648f41fc57d06
ALEXSANDER2002 <alex13filipi@gmail.com>
2025-03-30 23:48:51 -0300

    Volta a usar a biblioteca @djpfs/react-vlibras no componente cliente

--- components/VLibrasWidget.tsx
+++ components/VLibrasWidget.tsx
@@ -1,31 +1,7 @@
 'use client'
 
-import Script from 'next/script'
-import { useEffect } from 'react'
+import VLibras from '@djpfs/react-vlibras'
 
 export default function VLibrasWidget() {
-  useEffect(() => {
-    return () => {
-      // Cleanup on unmount
-    }
-  }, [])
-
-  return (
-    <>
-      <div data-vw="plugin" className="enabled">
-        <div data-vw-access-button className="active"></div>
-        <div data-vw-plugin-wrapper>
-          <div className="vw-plugin-top-wrapper"></div>
-        </div>
-      </div>
-      <Script
-        src="https://vlibras.gov.br/app/vlibras-plugin.js"
-        onLoad={() => {
-          // @ts-ignore
-          new window.VLibras.Widget();
-        }}
-        strategy="afterInteractive"
-      />
-    </>
-  )
+  return <VLibras forceOnload={true} />
 } 
